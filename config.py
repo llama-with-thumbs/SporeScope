0b2bb79797089468fb1eca55061b7a368b48ba5c
Vivi Filitovich <vlad.filitovich@gmail.com>
2025-12-02 18:51:54 -0500

    feat: add 3 new plats

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ CIRCLE_COORDS = [
 
 # --- Experiment Metadata ---
 SUBSTRATE = "Agar plate 100mm diameter"
-CULTURE = ["Control", "black morel, liquid culture", "Entheogen Explosion"]
-PLATE_START_TIME = ["2025-11-29T11:47:02Z", "2025-12-01T11:20:15Z", "2025-12-01T21:40:00Z"]
+CULTURE = ["Control", "black morel, liquid culture", "Entheogen Explosion", "Golden Teacher Cubensis", "Golden Teacher Cubensis", "Golden Teacher Cubensis"]
+PLATE_START_TIME = ["2025-11-29T11:47:02Z", "2025-12-01T11:20:15Z", "2025-12-01T21:40:00Z", "2025-12-02T18:51:00Z", "2025-12-02T18:52:30Z", "2025-12-02T18:53:45Z"]
 # --- Identification ---
 CHAMBER = "CHA-8BEA5D1"
-PLATE_ID = ["SMP-9414B8", "SMP-837C0C", "SMP-9A8231"]
+PLATE_ID = ["SMP-9414B8", "SMP-837C0C", "SMP-9A8231", "SMP-7C43C2", "SMP-85A40C", "SMP-2EEFFF"]
