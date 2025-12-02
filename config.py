75786542fab27571b17a4fb10df096047963abd1
Vivi Filitovich <vlad.filitovich@gmail.com>
2025-12-01 21:41:57 -0500

    feat: add another sample

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ CIRCLE_COORDS = [
 
 # --- Experiment Metadata ---
 SUBSTRATE = "Agar plate 100mm diameter"
-CULTURE = ["Control", "black morel, liquid culture"]
-PLATE_START_TIME = ["2024-11-29T11:47:02Z", "2024-12-01T11:20:15Z"]
+CULTURE = ["Control", "black morel, liquid culture", "Entheogen Explosion"]
+PLATE_START_TIME = ["2024-11-29T11:47:02Z", "2024-12-01T11:20:15Z", "2024-12-01T21:40:00Z"]
 # --- Identification ---
 CHAMBER = "CHA-8BEA5D1"
-PLATE_ID = ["SMP-9414B8", "SMP-837C0C"]
+PLATE_ID = ["SMP-9414B8", "SMP-837C0C", "SMP-9A8231"]
