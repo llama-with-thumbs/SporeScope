c9830740b0c21fcf00f5ab9a74bd58c83df9fadb
Vivi Filitovich <vlad.filitovich@gmail.com>
2025-12-11 21:57:09 -0500

    feat: renew 3 samples

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ CIRCLE_COORDS = [
 # --- Experiment Metadata ---
 SUBSTRATE = "Agar plate 100mm diameter"
 CULTURE = ["Control", "black morel, liquid culture", "Entheogen Explosion", "Golden Teacher Cubensis", "Golden Teacher Cubensis", "Golden Teacher Cubensis"]
-PLATE_START_TIME = ["2025-11-29T11:47:02Z", "2025-12-01T11:20:15Z", "2025-12-01T21:40:00Z", "2025-12-02T18:51:00Z", "2025-12-02T18:52:30Z", "2025-12-02T18:53:45Z"]
+PLATE_START_TIME = ["2025-11-29T11:47:02Z", "2025-12-01T11:20:15Z", "2025-12-01T21:40:00Z", "2025-12-11T21:50:00Z", "2025-12-11T21:51:00Z", "2025-12-11T21:52:00Z"]
 # --- Identification ---
 CHAMBER = "CHA-8BEA5D1"
-PLATE_ID = ["SMP-9414B8", "SMP-837C0C", "SMP-9A8231", "SMP-7C43C2", "SMP-85A40C", "SMP-2EEFFF"]
+PLATE_ID = ["SMP-9414B8", "SMP-837C0C", "SMP-9A8231", "SMP-38F85F", "SMP-0B8A34", "SMP-B9B2D3"]
